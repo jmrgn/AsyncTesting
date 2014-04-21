8407a50bf8fa05ddbd24d74913dea4e5e02e7c47
James Morgan <j.a.mrgn@gmail.com>
2014-04-21 17:14:18 -0400

    Business layer tests

---
+++ test/AsyncTesting.Test/BusinessLayerTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AsyncTesting.BusinessLogic;
+using AsyncTesting.DataAccess;
+using NUnit.Framework;
+
+namespace AsyncTesting.Test
+{
+    [TestFixture]
+    public class BusinessLayerTests
+    {
+        BusinessLayer businessLayer;
+        DataLayer dataLayer;
+
+        [SetUp]
+        public void SetUp()
+        {
+            dataLayer = new DataLayer(); 
+            businessLayer = new BusinessLayer(dataLayer);
+        }
+
+        // STEP 1: Actually execute task, using await in test code
+        // STEP 2: Ghetto mock w/a delegate
+        // STEP 3: Use Task.FromResult, Moq.ReturnsAsync a civilized human being
+    }
+}
